cfcb12d0526a138834c54d3ec89f7bc3d6f2dd3d
marcelaf7 <marcelaf@rams.colostate.edu>
2018-10-15 20:26:08 -0600

    removed src directory

--- edu/colostate/cs/cs414/method_men/jungle/client/Main.java
+++ edu/colostate/cs/cs414/method_men/jungle/client/Main.java
@@ -3,5 +3,5 @@ package edu.colostate.cs.cs414.method_men.jungle.client;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
+     }
 }
