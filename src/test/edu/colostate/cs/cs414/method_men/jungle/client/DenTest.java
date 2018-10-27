5c78cd025b60809edc8a6bbe919ed2a6657eae95
jgoing <jgoing@rams.colostate.edu>
2018-10-27 11:51:49 -0600

    cat, den, dog, elephant tests

--- src/test/edu/colostate/cs/cs414/method_men/jungle/client/DenTest.java
+++ src/test/edu/colostate/cs/cs414/method_men/jungle/client/DenTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class DenTest {
 
     @Test
-    void getAttribute() {
+    void constructor() {
+        Den den = new Den();
+        Den test = new Den();
+        assertTrue(den.getAttribute() == test.getAttribute());
     }
 }
--- src/test/edu/colostate/cs/cs414/method_men/jungle/client/DogTest.java
+++ src/test/edu/colostate/cs/cs414/method_men/jungle/client/DogTest.java
@@ -7,30 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class DogTest {
 
     @Test
-    void setLocation() {
-    }
-
-    @Test
-    void getLocation() {
-    }
-
-    @Test
-    void getRow() {
-    }
-
-    @Test
-    void getCol() {
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void getRank() {
-    }
-
-    @Test
-    void isRat() {
+    void constructor() {
+        Dog test1 = new Dog("white");
+        Dog test2 = new Dog("black");
+        int[] loc1 = test1.getLocation();
+        assertTrue(loc1[0] == 1 && loc1[1] == 1);
+        int[] loc2 = test2.getLocation();
+        assertTrue(loc2[0] == 7 && loc2[1] == 5);
     }
 }
