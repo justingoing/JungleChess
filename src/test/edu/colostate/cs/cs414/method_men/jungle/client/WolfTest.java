38287e07478cfb97138cf81f712a0d887270035f
Connor13C <connorbcarey@hotmail.com>
2018-11-01 12:22:40 -0600

    added WolfTest closes #62

--- src/test/edu/colostate/cs/cs414/method_men/jungle/client/WolfTest.java
+++ src/test/edu/colostate/cs/cs414/method_men/jungle/client/WolfTest.java
@@ -5,32 +5,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WolfTest {
+    Wolf wolf;
 
     @Test
     void setLocation() {
+        wolf = new Wolf("black");
+        Location location = wolf.getLocation();
+        wolf.setLocation(0,0);
+        assertTrue(wolf.getLocation()!=location);
     }
 
     @Test
     void getLocation() {
+        wolf = new Wolf("black");
+        assertTrue(wolf.getLocation()!=null);
+    }
+
+    @Test
+    void getLocation2() {
+        wolf = new Wolf("purple");
+        assertTrue(wolf.getLocation()==null);
     }
 
     @Test
     void getRow() {
+        wolf = new Wolf("black");
+        assertTrue(wolf.getRow()==6);
     }
 
     @Test
     void getCol() {
+        wolf = new Wolf("black");
+        assertTrue(wolf.getCol()==2);
     }
 
     @Test
     void getName() {
+        wolf = new Wolf("black");
+        assertTrue(wolf.getName()=="Wolf");
     }
 
     @Test
     void getRank() {
+        wolf = new Wolf("black");
+        assertTrue(wolf.getRank()==3);
     }
 
     @Test
-    void isRat() {
+    void iswolf() {
+        wolf = new Wolf("black");
+        assertFalse(wolf.isRat());
     }
 }
