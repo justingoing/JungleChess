61f0efb5ee5a7d568586baa2ae6852f07b9acfab
Zane Womeldorph <zwomeldorph@gmail.com>
2018-11-11 19:59:19 -0700

    threaded send and receive in both client and server, still need new threads for each client in server

--- src/main/edu/colostate/cs/cs414/method_men/jungle/client/server/Client.java
+++ src/main/edu/colostate/cs/cs414/method_men/jungle/client/server/Client.java
@@ -1,15 +1,19 @@
 package edu.colostate.cs.cs414.method_men.jungle.client.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
+public class Client implements Runnable{
 
     private Socket socket;
     private Scanner scanner;
+    private PrintWriter send;
+    private BufferedReader receive;
 
     private Client(InetAddress address, int port) throws Exception{
         this.socket = new Socket(address,port);
@@ -20,12 +24,27 @@ public class Client {
         String msg;
         while(true){
             msg = scanner.nextLine();
-            PrintWriter send = new PrintWriter(this.socket.getOutputStream(), true);
+            send = new PrintWriter(this.socket.getOutputStream(), true);
             send.println(msg);
             send.flush();
             System.out.println("Message sent: " + msg);
         }
 
+    }
+
+    public void run(){
+        String msg = null;
+        System.out.println("Receive thread started");
+        while (true) {
+            try {
+                //Buffered Reader reads from socket
+                receive = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while ((msg = receive.readLine()) != null) {
+                    System.out.println("Message received: " + msg);
+                }
+            } catch (Exception e) {
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception{
@@ -34,9 +53,9 @@ public class Client {
         Client client = new Client(address, 2000);
         System.out.println("Connected to server at " + address.toString() + " on port 2000");
         System.out.println("Type a message...");
+        Thread t1 = new Thread(client, "Receive");
+        t1.start();
         client.start();
-
-
 
     }
 }
--- src/main/edu/colostate/cs/cs414/method_men/jungle/client/server/TCPServer.java
+++ src/main/edu/colostate/cs/cs414/method_men/jungle/client/server/TCPServer.java
@@ -1,33 +1,65 @@
 package edu.colostate.cs.cs414.method_men.jungle.client.server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class TCPServer {
+
+public class TCPServer implements Runnable{
 
     private ServerSocket server;
+    private Socket client;
+    private Scanner scanner;
 
 
     //Creates server, listens on port
     public TCPServer(int port) throws Exception{
         this.server = new ServerSocket(port);
+        this.scanner = new Scanner(System.in);
     }
 
-    private void listen() throws Exception{
+    private void listen() throws Exception {
         String msg = null;
-        Socket client = this.server.accept();
+        this.client = this.server.accept();
         System.out.println("Socket Accepted on port: " + client.getLocalPort());
-        String clientAddress = client.getInetAddress().getHostAddress();
-        //Buffered Reader reads from socket
-        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        while((msg = in.readLine()) != null) {
-            System.out.println("Message received from: " + clientAddress + ": " + msg);
-            //System.out.println(msg);
-            //msg = null;
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                String msg = null;
+                System.out.println("Receive thread started");
+                while (true) {
+                    String clientAddress = client.getInetAddress().getHostAddress();
+                    try {
+                        //Buffered Reader reads from socket
+                        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        while ((msg = in.readLine()) != null) {
+                            System.out.println("Message received from: " + clientAddress + ": " + msg);
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+            }
+
+        };
+        t.start();
+    }
+
+    private void send() throws IOException{
+        System.out.println("Send thread started");
+        String msg = null;
+        while(true){
+            msg = scanner.nextLine();
+            PrintWriter send = new PrintWriter(this.client.getOutputStream(), true);
+            send.println(msg);
+            send.flush();
+            System.out.println("Message sent: " + msg);
         }
+
     }
 
     public InetAddress getSocketAddress(){
@@ -38,10 +70,20 @@ public class TCPServer {
         return this.server.getLocalPort();
     }
 
+    //threading
+    public void run(){
+        try{
+            this.send();
+        }catch(IOException e){}
+    }
+
     public static void main(String[] args) throws Exception{
         TCPServer server = new TCPServer(2000);
         System.out.println("Server Started");
         System.out.println("Waiting for connection");
         server.listen();
+        Thread t1 = new Thread(server, "send");
+        t1.start();
+
     }
 }
