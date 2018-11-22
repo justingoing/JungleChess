7f41f76d3f58849028653b9b3ae416ab2964fcb9
Julien Pecquet <jjpk@rams.colostate.edu>
2018-11-22 05:31:29 -0700

    * Added a very complex and subtle javadoc for Player.java.

--- src/main/edu/colostate/cs/cs414/method_men/jungle/client/Player.java
+++ src/main/edu/colostate/cs/cs414/method_men/jungle/client/Player.java
@@ -1,9 +1,13 @@
 package edu.colostate.cs.cs414.method_men.jungle.client;
 
+/**
+ * Player class is responsible for which color the player was assigned: red or blue.
+ * That's it.
+ */
 public class Player {
-    String color;
+    private String color;
 
-    public Player(String color) {
+    Player(String color) {
         this.color = color;
     }
 
