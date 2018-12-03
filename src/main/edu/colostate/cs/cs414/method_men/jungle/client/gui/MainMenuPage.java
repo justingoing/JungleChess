3976a5ddd4b6c8cf5ac0b71f1a52b5c7ba4e8675
Julien Pecquet <jjpk@rams.colostate.edu>
2018-12-02 18:22:17 -0700

    * MainMenuPage.java changes: Added a background to be consistent with other menus. Created a few new buttons: Profile, Games, Invitations. Rearranged the buttons to fit a grid. Created an inset (margin) around the buttons to show the new background.
    As of now, the new buttons don't do anything. The switch statement is there for when the functionality is added, but those pages don't exist quite yet.

--- src/main/edu/colostate/cs/cs414/method_men/jungle/client/gui/MainMenuPage.java
+++ src/main/edu/colostate/cs/cs414/method_men/jungle/client/gui/MainMenuPage.java
@@ -1,28 +1,103 @@
 package edu.colostate.cs.cs414.method_men.jungle.client.gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainMenuPage extends Page implements ActionListener {
-    public MainMenuPage(GUI frame) {
+    private Image background;
+
+    MainMenuPage(GUI frame) {
+        //Start off by calling super constructor, initializing the grids and getting the background.
         super(frame);
-        JButton b = new JButton("Start game");
-        b.setBounds(0, 0, 20, 20);
-        b.setActionCommand("StartGame");
-        b.addActionListener(this);
-        add(b);
+        this.background = Toolkit.getDefaultToolkit().createImage("src/Images/jungle.jpg");
+        GridBagLayout gridbag = new GridBagLayout();
+        this.setLayout(gridbag);
+        GridBagConstraints c = new GridBagConstraints();
+
+
+        //My Profile button
+        JButton profile = new JButton("Profile");
+        profile.setActionCommand("Profile");
+        profile.addActionListener(this);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(25,25,0,0);
+        gridbag.setConstraints(profile,c);
+        add(profile);
+
+        //Current games
+        JButton games = new JButton("Games");
+        games.setActionCommand("Games");
+        games.addActionListener(this);
+        c.gridx = 1;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(25,0,0,0);
+        gridbag.setConstraints(games,c);
+        add(games);
+
+        //Invitations
+        JButton invitations = new JButton("Invitations");
+        invitations.setActionCommand("Invitations");
+        invitations.addActionListener(this);
+        c.gridx = 2;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(25,0,0,25);
+        gridbag.setConstraints(invitations,c);
+        add(invitations);
+
+        //Start Game button
+        JButton startGame = new JButton("Start a new game");
+        startGame.setActionCommand("StartGame");
+        startGame.addActionListener(this);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 40;
+        c.weightx = 0.0;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(5,25,25,25);
+        gridbag.setConstraints(startGame,c);
+        add(startGame);
+
+        //This is probably unnecessary
+        c.insets = new Insets(0,0,0,0);
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         switch (actionEvent.getActionCommand()) {
             case "StartGame":
+                //TODO: Go to outgoings invitations page
+                //frame.changePageTo(new InvitationsPage(frame));
+                //TODO: Change below case to be an offline version of the game, or remove it.
                 frame.changePageTo(new GamePage(frame));
                 break;
-
+            case "Profile":
+                //TODO: Go to profile page
+                //frame.changePageTo(new ProfilePage(frame));
+                break;
+            case "Games":
+                //TODO: Go to game page
+                //frame.changePageTo(new CurrentGamesPage(frame));
+                break;
+            case "Invitations":
+                //TODO: Go to invitations page
+                //frame.changePageTo(new InvitationsPage(frame));
+                break;
             default:
                 break;
         }
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.drawImage(this.background, 0, 0, this);
+        g2d.dispose();
     }
 }
