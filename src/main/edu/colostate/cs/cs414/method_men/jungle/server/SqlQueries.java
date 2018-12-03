7c01ab75435d0f65743f50f63a375e8717bd7a0f
Connor Carey <connorbcarey@hotmail.com>
2018-12-03 13:53:56 -0700

    Streamlined field entry

--- src/main/edu/colostate/cs/cs414/method_men/jungle/server/SqlQueries.java
+++ src/main/edu/colostate/cs/cs414/method_men/jungle/server/SqlQueries.java
@@ -13,167 +13,167 @@ public interface SqlQueries {
 
     /**
      * Adds Username and Password to user table.
-     * @param input Username: not null String.
-     * @param input2 Password: not null String.
+     * @param Username: not null String.
+     * @param Password: not null String.
      * @return True if added. False otherwise.
      */
-    @SqlUpdate("INSERT INTO user(Username,Password) values(:input, :input2);")
-    boolean addUser(@Bind("input") String input, @Bind("input2")String input2);
+    @SqlUpdate("INSERT INTO user(Username,Password) values(:Username, :Password);")
+    boolean addUser(@Bind("Username") String Username, @Bind("Password")String Password);
 
     /**
      * Adds User1, User2, State, and Start_Date to match_state table field ID is auto generated
-     * @param input User1: not null String and must be in user table
-     * @param input2 User2: not null String and must be in user table
-     * @param input3 State: String
-     * @param input4 Start_Date String in format DDMMYYYYTTTT
+     * @param User1: not null String and must be in user table
+     * @param User2: not null String and must be in user table
+     * @param State: String
+     * @param Start_Date String in format DDMMYYYYTTTT
      * @return ID: Long unique to match_state row
      */
-    @SqlUpdate("INSERT INTO match_state(User1,User2,State,Start_Date) values(:input,:input2,:input3,:input4);")
+    @SqlUpdate("INSERT INTO match_state(User1,User2,State,Start_Date) values(:User1,:User2,:State,:Start_Date);")
     @GetGeneratedKeys("ID")
-    Long addMatchState(@Bind("input") String input, @Bind("input2")String input2, @Bind("input3") String input3, @Bind("input4")String input4);
+    Long addMatchState(@Bind("User1") String User1, @Bind("User2")String User2, @Bind("State") String State, @Bind("Start_Date")String Start_Date);
 
     /**
      * Adds Winner, Loser, Game_Start, and Game_End to match_record table
-     * @param input Winner: not null String and must be in user table
-     * @param input2 Loser: not null String and must be in user table
-     * @param input3 Game_Start String in format DDMMYYYYTTTT
-     * @param input4 Game_End String in format DDMMYYYYTTTT
+     * @param Winner: not null String and must be in user table
+     * @param Loser: not null String and must be in user table
+     * @param Game_Start String in format DDMMYYYYTTTT
+     * @param Game_End String in format DDMMYYYYTTTT
      * @return ID: Long unique to match_record row
      */
-    @SqlUpdate("INSERT INTO match_record(Winner,Loser,Game_Start,Game_End) values(:input,:input2,:input3,:input4);")
+    @SqlUpdate("INSERT INTO match_record(Winner,Loser,Game_Start,Game_End) values(:Winner,:Loser,:Game_Start,:Game_End);")
     @GetGeneratedKeys("ID")
-    Long addMatchRecord(@Bind("input") String input, @Bind("input2")String input2, @Bind("input3") String input3, @Bind("input4")String input4);
+    Long addMatchRecord(@Bind("Winner") String Winner, @Bind("Loser")String Loser, @Bind("Game_Start") String Game_Start, @Bind("Game_End")String Game_End);
 
     /**
      * Adds Inviter and Invitee to match_invite table
-     * @param input Inviter: not null String and must be in user table
-     * @param input2 Invitee: not null String and must be in user table
+     * @param Inviter: not null String and must be in user table
+     * @param Invitee: not null String and must be in user table
      * @return True if added. False otherwise.
      */
-    @SqlUpdate("INSERT INTO match_invite(Inviter,Invitee) values(:input,:input2);")
-    boolean addMatchInvite(@Bind("input") String input, @Bind("input2")String input2);
+    @SqlUpdate("INSERT INTO match_invite(Inviter,Invitee) values(:Inviter,:Invitee);")
+    boolean addMatchInvite(@Bind("Inviter") String Inviter, @Bind("Invitee")String Invitee);
 
     /**
      * Searches for Username in user table
-     * @param search Username String
+     * @param Username String
      * @return String with Username if in table or empty List if not in table
      */
-    @SqlQuery("SELECT Username FROM user WHERE Username=:search;")
-    String searchUser(@Bind("search") String search);
+    @SqlQuery("SELECT Username FROM user WHERE Username=:Username;")
+    String searchUser(@Bind("Username") String Username);
 
     /**
      * Searches for exact Username and Password in user table
-     * @param search Username: String
-     * @param search2 Password: String
+     * @param Username: String
+     * @param Password: String
      * @return String Username if in table or null if not in table
      */
-    @SqlQuery("SELECT Username FROM user WHERE Username=:search AND Password=:search2;")
-    String searchUserPassword(@Bind("search") String search, @Bind("search2")String search2);
+    @SqlQuery("SELECT Username FROM user WHERE Username=:Username AND Password=:Password;")
+    String searchUserPassword(@Bind("Username") String Username, @Bind("Password")String Password);
 
     /**
      * Searches for match_state by matching ID
-     * @param search ID: Long unique in match_state
+     * @param ID: Long unique in match_state
      * @return String State: String representation of the state of the match in match_state table
      */
-    @SqlQuery("SELECT State FROM match_state WHERE ID=:search;")
-    String searchStateMatchState(@Bind("search") Long search);
+    @SqlQuery("SELECT State FROM match_state WHERE ID=:ID;")
+    String searchStateMatchState(@Bind("ID") Long ID);
 
 
     /**
      * Searches for Start_Date in match_state table by matching ID
-     * @param search ID: Long unique
+     * @param ID: Long unique
      * @return String Start_Date: not null String in format DDMMYYYYTTTT
      */
-    @SqlQuery("SELECT Start_Date FROM match_state WHERE ID=:search;")
-    String searchStartDateMatchState(@Bind("search") Long search);
+    @SqlQuery("SELECT Start_Date FROM match_state WHERE ID=:ID;")
+    String searchStartDateMatchState(@Bind("ID") Long ID);
 
     /**
      * Searches for row in match_record table matching Winner
-     * @param input Winner: not null String and must be in user table
+     * @param Winner: not null String and must be in user table
      * @return List<Long>: IDs from rows from match_record table with matching Winners
      */
-    @SqlQuery("SELECT ID from match_record WHERE Winner=:input;")
-    List<Long> searchWinnerMatchRecord(@Bind("input") String input);
+    @SqlQuery("SELECT ID from match_record WHERE Winner=:Winner;")
+    List<Long> searchWinnerMatchRecord(@Bind("Winner") String Winner);
 
     /**
      * Searches for row in match_record table matching Winner
-     * @param input Loser: not null String and must be in user table
+     * @param Loser: not null String and must be in user table
      * @return List<Long>: IDs from rows from match_record table with matching Winners
      */
-    @SqlQuery("SELECT ID from match_record WHERE Winner=:input;")
-    List<Long> searchLoserMatchRecord(@Bind("input") String input);
+    @SqlQuery("SELECT ID from match_record WHERE Loser=:Loser;")
+    List<Long> searchLoserMatchRecord(@Bind("Loser") String Loser);
 
     /**
      * Searches for Inviter and Invitee in match_invite table by matching Inviter
-     * @param input Inviter: not null String and must be in user table
+     * @param Inviter: not null String and must be in user table
      * @return List<String>: Invitees from match_invite table
      */
-    @SqlQuery("SELECT Invitee from match_invite WHERE Inviter=:input;")
-    List<String> searchMatchInviter(@Bind("input") String input);
+    @SqlQuery("SELECT Invitee from match_invite WHERE Inviter=:Inviter;")
+    List<String> searchMatchInviter(@Bind("Inviter") String Inviter);
 
     /**
      * Searches for Inviter and Invitee in match_invite table by matching Invitee
-     * @param input Invitee: not null String and must be in user table
+     * @param Invitee: not null String and must be in user table
      * @return List<String>: Inviters from match_invite table
      */
-    @SqlQuery("SELECT Inviter from match_invite WHERE Invitee=:input;")
-    List<String> searchMatchInvitee(@Bind("input") String input);
+    @SqlQuery("SELECT Inviter from match_invite WHERE Invitee=:Invitee;")
+    List<String> searchMatchInvitee(@Bind("Invitee") String Invitee);
 
     /**
      * Updates State in match_state table matching ID
-     * @param input State: String representation of the state of the match in match_state table
-     * @param input2 ID: Long unique
+     * @param State: String representation of the state of the match in match_state table
+     * @param ID: Long unique
      * @return True if updated. False otherwise.
      */
-    @SqlUpdate("UPDATE match_state SET State=:input WHERE ID=:input2")
-    boolean updateMatchState(@Bind("input") String input, @Bind("input2")Long input2);
+    @SqlUpdate("UPDATE match_state SET State=:State WHERE ID=:ID")
+    boolean updateMatchState(@Bind("State") String State, @Bind("ID")Long ID);
 
     /**
      * Deletes Username from user table matching Username
-     * @param input Username: String
+     * @param Username: String
      * @return True if deleted. False otherwise.
      */
-    @SqlUpdate("DELETE FROM user WHERE Username=:input;")
-    boolean deleteUser(@Bind("input") String input);
+    @SqlUpdate("DELETE FROM user WHERE Username=:Username;")
+    boolean deleteUser(@Bind("Username") String Username);
 
     /**
      * Deletes match_state row from match_state table matching ID
-     * @param input ID: Long unique
+     * @param ID: Long unique
      * @return True if deleted. False otherwise.
      */
-    @SqlUpdate("DELETE FROM match_state WHERE ID=:input")
-    boolean deleteMatchState(@Bind("input") Long input);
+    @SqlUpdate("DELETE FROM match_state WHERE ID=:ID")
+    boolean deleteMatchState(@Bind("ID") Long ID);
 
     /**
      * Deletes match_record row from match_record table matching ID
-     * @param input ID: Long unique
+     * @param ID: Long unique
      * @return True if deleted. False otherwise.
      */
-    @SqlUpdate("DELETE FROM match_record WHERE ID=:input;")
-    boolean deleteMatchRecord(@Bind("input") Long input);
+    @SqlUpdate("DELETE FROM match_record WHERE ID=:ID;")
+    boolean deleteMatchRecord(@Bind("ID") Long ID);
 
     /**
      * Deletes match_invite row from match_invite table matching Inviter
-     * @param input Inviter: not null String and must be in user table
+     * @param Inviter: not null String and must be in user table
      * @return True if deleted. False otherwise.
      */
-    @SqlUpdate("DELETE FROM match_invite WHERE Inviter=:input;")
-    boolean deleteMatchInvite(@Bind("input") String input);
+    @SqlUpdate("DELETE FROM match_invite WHERE Inviter=:Inviter;")
+    boolean deleteMatchInvite(@Bind("Inviter") String Inviter);
 
     /**
      * Changes Username to deletedUser in match_record rows matching Winner
-     * @param input Winner: not null String and must be in user table
+     * @param Winner: not null String and must be in user table
      * @return True if deleted. False otherwise.
      */
-    @SqlUpdate("UPDATE match_record SET Winner='deletedUser' WHERE Winner=:input")
-    boolean deleteWinnerMatchRecord(@Bind("input") String input);
+    @SqlUpdate("UPDATE match_record SET Winner='deletedUser' WHERE Winner=:Winner")
+    boolean deleteWinnerMatchRecord(@Bind("Winner") String Winner);
 
     /**
      * Changes Username to deletedUser in match_record rows matching Loser
-     * @param input Loser: not null String and must be in user table
+     * @param Loser: not null String and must be in user table
      * @return True if deleted. False otherwise.
      */
-    @SqlUpdate("UPDATE match_record SET Loser='deletedUser' WHERE Loser=:input")
-    boolean deleteLoserMatchRecord(@Bind("input") String input);
+    @SqlUpdate("UPDATE match_record SET Loser='deletedUser' WHERE Loser=:Loser")
+    boolean deleteLoserMatchRecord(@Bind("Loser") String Loser);
 
 }
