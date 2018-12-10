a5205721e65c0a8e9e533d3beb2045bb5025fcd1
jgoing <jgoing@rams.colostate.edu>
2018-12-09 17:42:54 -0700

    Receive refactored. Now for doc.

--- src/main/edu/colostate/cs/cs414/method_men/jungle/server/Receive.java
+++ src/main/edu/colostate/cs/cs414/method_men/jungle/server/Receive.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import static java.lang.Math.toIntExact;
 
 public class Receive extends Thread{
 
@@ -15,225 +14,229 @@ public class Receive extends Thread{
     private TCPServer server;
     private long gameID;
 
-    public Receive(Socket socket, TCPServer server, long gameID) throws IOException{
+    private Receive(Socket socket, TCPServer server, long gameID) throws IOException{
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.socket = socket;
         this.server = server;
         this.gameID = gameID;
     }
 
-    public void receive(){
-        String msg = null;
-        System.out.println("Receive thread started");
+    private void receive(){
+        String msg;
         try {
-            //Buffered Reader reads from socket
             while ((msg = in.readLine()) != null) {
                 System.out.println("Message received: " + msg);
                 //Parse input into string array
                 String[] message = parseReceive(msg);
                 respondToInput(message, msg);
             }
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
     }
 
-    public String[] parseReceive(String msg) {
-        String [] items = msg.split(" ");
-        return items;
+    private String[] parseReceive(String msg) {
+        return msg.split(" ");
     }
 
-    public void respondToInput(String [] message, String wholeString){
-        //if logging in, do some stuff to send to database to authenticate, etc.
-        //TODO
+    private void respondToInput(String [] message, String wholeString){
         if(message[0].equals("login")){
-            //will need to check/call authentication methods here
-            if(authenticateUser(message[1], message[2])){
-                try{
-                    Send send = new Send(this.socket, this.server, gameID);
-                    send.sendLoginResponse(true);
-                    User user = new User(message[1], this.socket);
-                    server.addUser(user);
-                }catch(Exception e){}
-            }
-            else{
-                try {
-                    Send send = new Send(this.socket, this.server, gameID);
-                    send.sendLoginResponse(false);
-                }catch (Exception e) {}
-            }
+            processLogin(message);
         }
         if(message[0].equals("register")){
-            if(registerUser(message[1], message[2])){
-                try{
-                    Send send = new Send(this.socket, this.server, gameID);
-                    send.sendRegisterResponse(true);
-                    User user = new User(message[1], this.socket);
-                    server.addUser(user);
-                }catch(Exception e){}
-            }
-            else{
-                try {
-                    Send send = new Send(this.socket, this.server, gameID);
-                    send.sendRegisterResponse(false);
-                }catch (Exception e) {}
-            }
+           processRegister(message);
         }
-
         if(message[0].equals("GameState")){
-            //send to database
-            server.getSQL().updateMatchState(wholeString, gameID);
-            //send to other client
-            String [] getOtherUser  = message[3].split(":");
-            try{
-                Send send = new Send(this.socket, this.server, gameID);
-                for(User u: server.getUsers()){
-                    if(u.getUsername().equals(getOtherUser[1])){
-                        u.send(wholeString);
-                        break;
-                    }
-                }
-            }catch(Exception e){}
+            processGameState(message, wholeString);
         }
-
         if(message[0].equals("Invite")) {
-            System.out.println("Invite Received Inviter = " + message[1] + " Invitee = " + message[2]);
-            System.out.println(server.getSQL().searchPairMatchInvite(message[1], message[2]));
-            boolean found = findUser(message[2]);
-            if (!found){
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    send.sendString("Fail");
-                }catch(Exception e){}
-            }
-            else if(!server.getSQL().searchPairMatchInvite(message[1], message[2]).isEmpty()){
-                //System.out.println(server.getSQL().searchPairMatchInvite(message[1], message[2]));
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    send.sendString("There");
-                }catch(Exception e){}
-            }
-            else if(server.getSQL().searchPairMatchInvite(message[1], message[2]).isEmpty()){
-                System.out.println("Found Invitee");
-                boolean value = addInvite(message[1], message[2]);
-                System.out.println("update? " + value);
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    send.sendString("Success");
-                }catch(Exception e){}
-            }
+            processInvite(message);
         }
         if(message[0].equals(("myInvites"))){
-            List<String> myInvites = server.getSQL().searchMatchInvitee(message[1]);
-            if(myInvites.isEmpty()){
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    send.sendString("Fail");
-                }catch (Exception e){}
-            }else{
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    String invites = "";
-                    for(int i = 0; i < myInvites.size(); i++){
-                        invites += myInvites.get(i);
-                        if(i < myInvites.size()-1){
-                            invites += " ";
-                        }
-                    }
-                    send.sendString(invites);
-                }catch (Exception e){}
-            }
+            processMyInvites(message);
         }
         if(message[0].equals(("mySentInvites"))){
-            List<String> mySentInvites = server.getSQL().searchMatchInviter(message[1]);
-            if(mySentInvites.isEmpty()){
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    send.sendString("Fail");
-                }catch (Exception e){}
-            }else{
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    String invites = "";
-                    for(int i = 0; i < mySentInvites.size(); i++){
-                        invites += mySentInvites.get(i);
-                        if(i < mySentInvites.size()-1){
-                            invites += " ";
-                        }
-                    }
-                    send.sendString(invites);
-                }catch (Exception e){}
-            }
+           processMySentInvites(message);
         }
-
         if(message[0].equals("AcceptInvite")){
-            String state = buildDefaultGameState(message[1], message[2]);
-            System.out.println("State = " + state);
-            String date = getDateTime();
-            System.out.println("datetime = " + date);
-            long matchID = server.getSQL().addMatchState(message[1], message[2], state, date);
-            System.out.println("match added with id: " + matchID);
-            boolean deleted = server.getSQL().deleteMatchInvite(message[1]);
-            System.out.println("Deleted = " + deleted);
-            //do we need to send matchID back to client???
+            processAcceptInvite(message);
         }
-
         if(message[0].equals("RejectInvite")){
-            boolean deleted = server.getSQL().deleteMatchInvite(message[1]);
-            System.out.println("Deleted = " + deleted);
+            server.getSQL().deleteMatchInvite(message[1]);
         }
-
         if(message[0].equals("myGames")){
-            System.out.println("searching for games with " + message[1]);
-            List<Long> myGames = getUser1User2ID(message[1]);
-            System.out.println("searched games");
-            if(myGames.isEmpty()){
-                System.out.println("no Games");
-            }
-            else{
-                String games = "";
-                System.out.println("found games");
-                System.out.println(myGames);
-                for(int i = 0; i < myGames.size(); i++){
-                    String u1 = server.getSQL().searchMatchUser1FromID(myGames.get(i));
-                    String u2 = server.getSQL().searchMatchUser2FromID(myGames.get(i));
-                    String id = myGames.get(i).toString();
-                    String game = u1 + "," + u2 + "," + id;
-                    games += game + " ";
-                }
-                System.out.println(games);
-                try{
-                    Send send = new Send(this.socket, this.server, this.gameID);
-                    send.sendString(games);
-                }catch(Exception e){}
-            }
-
+            processMyGame(message);
         }
-
         if(message[0].equals("GetState")){
-            int id = Integer.parseInt(message[1]);
-            Long id1 = new Long(id);
-            String state = server.getSQL().searchStateMatchState(id1);
-            System.out.println(state);
-            try{
-                Send send = new Send(this.socket, this.server, this.gameID);
-                send.sendString(state);
-            }catch(Exception e){}
+            processGetState(message);
         }
-
-        //Oh dear Lord, here we go
-        if(message[0].equals("GameState")){
-            String state = buildGameState(message);
-            Long ID = Long.parseLong(message[1]);
-            //update state in DB if ont local game
-            if(ID > 0) {
-                boolean added = server.getSQL().updateMatchState(state, ID);
-                System.out.println("Added = " + added);
-            }
-            //Send state to other user???
-        }
-
     }
 
-    public List<Long> getUser1User2ID(String user1){
+    private void processLogin(String [] message){
+        if(authenticateUser(message[1], message[2])){
+            try{
+                Send send = new Send(this.socket, this.server, gameID);
+                send.sendLoginResponse(true);
+                User user = new User(message[1], this.socket);
+                server.addUser(user);
+            }catch(Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+        else{
+            try {
+                Send send = new Send(this.socket, this.server, gameID);
+                send.sendLoginResponse(false);
+            }catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
+        }
+    }
+
+    private void processRegister(String[] message){
+        if(registerUser(message[1], message[2])){
+            try{
+                Send send = new Send(this.socket, this.server, gameID);
+                send.sendRegisterResponse(true);
+                User user = new User(message[1], this.socket);
+                server.addUser(user);
+            }catch(Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+        else{
+            try {
+                Send send = new Send(this.socket, this.server, gameID);
+                send.sendRegisterResponse(false);
+            }catch (Exception e) {
+                System.out.println("Exception: " + e);
+            }
+        }
+    }
+
+    private void processGameState(String[] message, String wholeString){
+        server.getSQL().updateMatchState(wholeString, gameID);
+        String [] getOtherUser  = message[3].split(":");
+        try{
+            for(User u: server.getUsers()){
+                if(u.getUsername().equals(getOtherUser[1])){
+                    u.send(wholeString);
+                    break;
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Exception: " + e);
+        }
+    }
+
+    private void processInvite(String[] message){
+        boolean found = findUser(message[2]);
+        if (!found){
+            try{
+                Send send = new Send(this.socket, this.server, this.gameID);
+                send.sendString("Fail");
+            }catch(Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+        else if(!server.getSQL().searchPairMatchInvite(message[1], message[2]).isEmpty()){
+            try{
+                Send send = new Send(this.socket, this.server, this.gameID);
+                send.sendString("There");
+            }catch(Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+        else if(server.getSQL().searchPairMatchInvite(message[1], message[2]).isEmpty()){
+            try{
+                Send send = new Send(this.socket, this.server, this.gameID);
+                send.sendString("Success");
+            }catch(Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+    }
+
+    private void processMyInvites(String[] message){
+        List<String> myInvites = server.getSQL().searchMatchInvitee(message[1]);
+        myInvitesResponse(myInvites);
+    }
+
+    private void processMySentInvites(String[] message){
+        List<String> mySentInvites = server.getSQL().searchMatchInviter(message[1]);
+        myInvitesResponse(mySentInvites);
+    }
+
+    private void myInvitesResponse(List<String> mySentInvites){
+        if(mySentInvites.isEmpty()){
+            try{
+                Send send = new Send(this.socket, this.server, this.gameID);
+                send.sendString("Fail");
+            }catch (Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }else{
+            try{
+                Send send = new Send(this.socket, this.server, this.gameID);
+                String invites = "";
+                for(int i = 0; i < mySentInvites.size(); i++){
+                    invites += mySentInvites.get(i);
+                    if(i < mySentInvites.size()-1){
+                        invites += " ";
+                    }
+                }
+                send.sendString(invites);
+            }catch (Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+    }
+
+    private void processAcceptInvite(String[] message){
+        String state = buildDefaultGameState(message[1], message[2]);
+        String date = getDateTime();
+        server.getSQL().addMatchState(message[1], message[2], state, date);
+        server.getSQL().deleteMatchInvite(message[1]);
+    }
+
+    private void processMyGame(String[] message){
+        List<Long> myGames = getUser1User2ID(message[1]);
+        if(myGames.isEmpty()){
+            System.out.println("no Games");
+        }
+        else{
+            String games = "";
+            for(int i = 0; i < myGames.size(); i++){
+                String u1 = server.getSQL().searchMatchUser1FromID(myGames.get(i));
+                String u2 = server.getSQL().searchMatchUser2FromID(myGames.get(i));
+                String id = myGames.get(i).toString();
+                String game = u1 + "," + u2 + "," + id;
+                games += game + " ";
+            }
+            System.out.println(games);
+            try{
+                Send send = new Send(this.socket, this.server, this.gameID);
+                send.sendString(games);
+            }catch(Exception e){
+                System.out.println("Exception: " + e);
+            }
+        }
+    }
+
+    private void processGetState(String[] message){
+        int id = Integer.parseInt(message[1]);
+        Long id1 = new Long(id);
+        String state = server.getSQL().searchStateMatchState(id1);
+        System.out.println(state);
+        try{
+            Send send = new Send(this.socket, this.server, this.gameID);
+            send.sendString(state);
+        }catch(Exception e){
+            System.out.println("Exception: " + e);
+        }
+    }
+
+    private List<Long> getUser1User2ID(String user1){
         List<Long> out = SqlUtils.getJdbi().withHandle(h -> {
                     List<Long> name = h.createQuery("SELECT ID FROM match_state WHERE User1='" + user1 + "' OR User2='" + user1 +"'").mapTo(Long.class).list();
                     System.out.println(name);
@@ -243,52 +246,26 @@ public class Receive extends Thread{
         return out;
     }
 
-    public String buildDefaultGameState(String user1, String user2){
-        String state = "Winner:-1 NextTurn:Blue MoveCount:0 Red:7,0,0/6,0,6/4,1,1/2,1,5/1,2,0/5,2,2/3,2,4/8,2,6 Blue:8,6,0/3,6,2/5,6,4/1,6,6/2,7,1/4,7,5/6,8,0/7,8,6";
-        return state;
+    private String buildDefaultGameState(String user1, String user2){
+        return "Winner:-1 NextTurn:Blue MoveCount:0 Red:7,0,0/6,0,6/4,1,1/2,1,5/1,2,0/5,2,2/3,2,4/8,2,6 Blue:8,6,0/3,6,2/5,6,4/1,6,6/2,7,1/4,7,5/6,8,0/7,8,6";
     }
 
-    public String buildGameState(String [] message){
-        String s = "";
-        for (int i = 2; i < message.length; i++) {
-            s += message[i] + " ";
-        }
-        return s;
-    }
-
-    public String getDateTime(){
+    private String getDateTime(){
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyyhhmm");
-        String dateTime = dateFormat.format(date);
-        //System.out.println(dateTime);
-        return dateTime;
+        return dateFormat.format(date);
     }
 
-    public boolean findUser(String user){
-        if(null != server.getSQL().searchUser(user)){
-            return true;
-        }
-        else{
-            return false;
-        }
+    private boolean findUser(String user){
+        return null != server.getSQL().searchUser(user);
     }
 
-    public boolean addInvite(String user1, String user2){
-        return server.getSQL().addMatchInvite(user1, user2);
-    }
-
-    public boolean authenticateUser(String username, String password){
-        //if username does not exist
-        if(null == server.getSQL().searchUserPassword(username, password)){
-            return false;
-        }
-        else{
-            return true;
-        }
+    private boolean authenticateUser(String username, String password){
+        return null == server.getSQL().searchUserPassword(username, password);
     }
 
 
-    public boolean registerUser(String username, String password){
+    private boolean registerUser(String username, String password){
         String user = server.getSQL().searchUser(username);
         if (!username.equals(user)){
             server.getSQL().addUser(username, password);
